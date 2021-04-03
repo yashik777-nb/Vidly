2d6d03ca1603ea7293b499cdd66a2b86353fb404
Yash <yaswanth.iddibani@gmail.com>
2021-04-03 13:54:57 +0100

    Excersise 2 with Basics

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,14 +13,15 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!"};
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer> {
                 new Customer {Id = 1, Name = "Customer 1"},
                 new Customer {Id = 2, Name = "Customer 2"},
             };
 
-            var viewModel = new RandomMovieViewModel { 
-                Movie =  movie,
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
                 Customers = customers
             };
 
@@ -33,7 +34,7 @@ namespace Vidly.Controllers
             //var viewResult = new ViewResult();
             //viewResult.ViewData.Model = movie;
 
-            
+
 
             //return View(movie);
             //return Content("Hello World!");
@@ -47,20 +48,37 @@ namespace Vidly.Controllers
             return Content("id=" + id);
         }
 
-        // GET : Movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+        //// GET : Movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue) pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "name";
+
+
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
+
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue) pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "name";
+            var movies = GetMovies();
 
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return View(movies);
         }
 
-        [Route("movies/released/{year:regex(2015|2016)}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+
+        //[Route("movies/released/{year:regex(2015|2016)}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
+        private IEnumerable<Movie> GetMovies()
         {
-            return Content(year + "/" + month);
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
     }
 }
