021a9fbd4f5b34c973c0a8ee0e64c54930d72d4e
Yash <yaswanth.iddibani@gmail.com>
2021-04-04 09:53:14 +0100

    Section 3 - Excercise 2 - Customers from DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -35,7 +35,7 @@ namespace Vidly.Controllers
         //[Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
---
+++ Vidly/Migrations/202104032155247_UpdateCustomersData.cs
@@ -0,0 +1,18 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateCustomersData : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE Customers SET BirthDate = CAST('1992-01-01' AS DATETIME) WHERE ID = 1");
+            Sql("UPDATE Customers SET BirthDate = CAST('1998-01-01' AS DATETIME) WHERE ID = 2");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
